a7d6764e27eae0406336541e8c59140befb35435
Tervi test <Tervi@list.ru>
2023-07-29 13:45:57 +0300

    initial

--- src/test/java/ru/netology/web/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/web/AppOrderPositiveTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,32 +12,33 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppOrderPositiveTest {
+class CallbackTest {
     private WebDriver driver;
-    @BeforeAll
-    static void setUpAll() {
+
+    @BeforeAll // хук, который запускает драйвер
+    public static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
 
-
-
     @BeforeEach
-    void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        void setUp() {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            driver.get("http://localhost:9999");
     }
 
     @AfterEach
+        // выполняет действие после завершения теста
     void tearDown() {
-        driver.quit();
-        driver = null;
+        driver.quit(); // заканчивает работу
+        driver = null; // обнуляет его
     }
+
     @Test
-    public void shouldBeSuccessfulForm() {
+    void shouldBeSuccessfulForm() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79999999999");
         driver.findElement(By.className("checkbox__box")).click();
@@ -47,6 +47,5 @@ public class AppOrderPositiveTest {
         String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals(expected,actual);
-
     }
 }
