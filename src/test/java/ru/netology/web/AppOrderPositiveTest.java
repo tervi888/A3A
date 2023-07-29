e687618c4c63c8969044fb6481b3c1ab17a724ce
Tervi test <Tervi@list.ru>
2023-07-29 12:49:40 +0300

    initial

--- src/test/java/ru/netology/web/AppOrderPositiveTest.java
+++ src/test/java/ru/netology/web/AppOrderPositiveTest.java
@@ -39,14 +39,15 @@ public class AppOrderPositiveTest {
     }
     @Test
     public void shouldBeSuccessfulForm() {
-        driver.findElement(By.cssSelector("(data-test-id='name') input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("(data-test-id='phone') input")).sendKeys("+79999999999");
-        driver.findElement(By.cssSelector("checkbox__box")).click();
-        driver.findElement(By.cssSelector("button")).click();
-        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("data-test-id=order-success)")).getText().trim();
-        assertEquals(expected,actual);
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79999999999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
 
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals(expected,actual);
 
     }
 }
